7cb0c575d52ee6fe94f753dec2e51cdb22cf63db
Ruben-Dean <dean.caldeira@gmail.com>
2018-04-04 14:45:31 +0100

    going to pull, testing

---
+++ loopAdv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+x=1
+
+while [$x -le 30]
+do
+	if[[ 0 -eq "($x%3) + ($x%5)" ]]
+	then
+
+	echo "fizz buzz"
+	elif [[ 0 -eq "($x%5)" ]]
+	then
+
+	echo "buzz"
+	elif [[ 0 -eq "($x%3)" ]]
+	then
+
+	echo "fizz"
+	else
+	echo "$x"
+	fi
+	x=$(( $x + 1 ))
+done
